8abfffadea75110325c624f0f7f93f67cdf1cff2
ReeceSpr <reece.spragg@gmail.com>
2020-03-10 18:19:32 +1300

    ButtonNamesExternal

---
+++ src/app/buttons.ts
@@ -0,0 +1,21 @@
+export const btnNames = [
+  {
+    name: 'Projects',
+    link: "",
+  },
+  {
+    name: 'Resume',
+    link: "",
+  },
+  {
+    name: 'Contact',
+    link: "",
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
