0fe559c7fbb632f309c479d9e9e9586acaf28c8b
ReeceSpr <reece.spragg@gmail.com>
2020-03-11 00:12:11 +1300

    Resized for spacing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,11 @@ import { HelloComponent } from './hello.component';
 import { HeadingComponent } from './heading/heading.component';
 import { IconsComponent } from './icons/icons.component';
 import { LandingButtonsComponent } from './landing-buttons/landing-buttons.component';
+import { ScrolldownComponent } from './scrolldown/scrolldown.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HeadingComponent, IconsComponent, LandingButtonsComponent ],
+  declarations: [ AppComponent, HelloComponent, HeadingComponent, IconsComponent, LandingButtonsComponent, ScrolldownComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
