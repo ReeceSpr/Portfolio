95f4e548029683babce025f55502d0a117ec31e7
ReeceSpr <reece.spragg@gmail.com>
2020-03-10 18:17:20 +1300

    Layout Spacing on Landing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HeadingComponent } from './heading/heading.component';
+import { IconsComponent } from './icons/icons.component';
+import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HeadingComponent ],
+  declarations: [ AppComponent, HelloComponent, HeadingComponent, IconsComponent, ButtonComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
