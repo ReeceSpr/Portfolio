1e30406133850e2b9a14ac2cb7cc88a0fd926183
ReeceSpr <reece.spragg@gmail.com>
2020-03-16 09:46:41 +1300

    Added Dummy Content To Proj Tab

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,11 @@ import { HeadingComponent } from './heading/heading.component';
 import { IconsComponent } from './icons/icons.component';
 import { LandingButtonsComponent } from './landing-buttons/landing-buttons.component';
 import { ScrolldownComponent } from './scrolldown/scrolldown.component';
+import { ContentMobileComponent } from './content-mobile/content-mobile.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, HeadingComponent, IconsComponent, LandingButtonsComponent, ScrolldownComponent ],
+  declarations: [ AppComponent, HelloComponent, HeadingComponent, IconsComponent, LandingButtonsComponent, ScrolldownComponent, ContentMobileComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/content-mobile/content-mobile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content-mobile',
+  templateUrl: './content-mobile.component.html',
+  styleUrls: ['./content-mobile.component.css']
+})
+export class ContentMobileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
